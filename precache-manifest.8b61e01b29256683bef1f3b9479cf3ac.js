4fd534d34fbc2be6a6ab4955a58b74fc442e4322
champjiwrd <champ0952@hotmai.com>
2020-04-20 18:37:57 +0700

    Updates

---
+++ precache-manifest.8b61e01b29256683bef1f3b9479cf3ac.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2592a09238a9abe0bddf8b3b553bd551",
+    "url": "/Brewing-Coffee-Machine/index.html"
+  },
+  {
+    "revision": "e0284536d054b1ded409",
+    "url": "/Brewing-Coffee-Machine/static/css/main.acb29f5b.chunk.css"
+  },
+  {
+    "revision": "0e638bf5e45b6c7f924a",
+    "url": "/Brewing-Coffee-Machine/static/js/2.c8c0aa84.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/Brewing-Coffee-Machine/static/js/2.c8c0aa84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0284536d054b1ded409",
+    "url": "/Brewing-Coffee-Machine/static/js/main.ecfe2825.chunk.js"
+  },
+  {
+    "revision": "59b8a845ae67682c0f0d",
+    "url": "/Brewing-Coffee-Machine/static/js/runtime-main.2a95f267.js"
+  },
+  {
+    "revision": "7dc8f86cae5d1ab09dfb607da0a694c8",
+    "url": "/Brewing-Coffee-Machine/static/media/1.7dc8f86c.jpg"
+  },
+  {
+    "revision": "4d8147a43857a251215cb3dd1e735287",
+    "url": "/Brewing-Coffee-Machine/static/media/10.4d8147a4.jpg"
+  },
+  {
+    "revision": "1cfc1d932c7648923a956a43f1005b2e",
+    "url": "/Brewing-Coffee-Machine/static/media/11.1cfc1d93.jpg"
+  },
+  {
+    "revision": "bd713b39f5db5f29f5304a7d8b9cc224",
+    "url": "/Brewing-Coffee-Machine/static/media/2.bd713b39.jpg"
+  },
+  {
+    "revision": "bf3873f835d1c2983a858c5c40062397",
+    "url": "/Brewing-Coffee-Machine/static/media/3.bf3873f8.jpg"
+  },
+  {
+    "revision": "55765fefcab464150dbe26f345ec6c93",
+    "url": "/Brewing-Coffee-Machine/static/media/4.55765fef.jpg"
+  },
+  {
+    "revision": "17f619ae4c95e65f130b9975d949a077",
+    "url": "/Brewing-Coffee-Machine/static/media/5.17f619ae.jpg"
+  },
+  {
+    "revision": "60fb92b6cfbbe097042bff93dc1a0ba2",
+    "url": "/Brewing-Coffee-Machine/static/media/6.60fb92b6.jpg"
+  },
+  {
+    "revision": "a8397a4e4f2d783279a62331c0fbd757",
+    "url": "/Brewing-Coffee-Machine/static/media/7.a8397a4e.jpg"
+  },
+  {
+    "revision": "9dae2c29a3885db2550fa3edb06c8c86",
+    "url": "/Brewing-Coffee-Machine/static/media/8.9dae2c29.jpg"
+  },
+  {
+    "revision": "aed291c29bc43567ce5082c66e0c6233",
+    "url": "/Brewing-Coffee-Machine/static/media/9.aed291c2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2718604571b33ab6fd92e16f93dcbd69.js"
+  "/Brewing-Coffee-Machine/precache-manifest.8b61e01b29256683bef1f3b9479cf3ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Brewing-Coffee-Machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
